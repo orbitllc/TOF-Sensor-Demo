9483405a1702773d124b1109465676763589ae40
orbitllc <alexbowen@orbitllc.site>
2023-10-27 23:52:50 -0400

    Implemented my new "magicalStateMap" algorithm, which uses two stacks and replaces the finite state machine.

--- src/PeopleCounter.cpp
+++ src/PeopleCounter.cpp
@@ -11,7 +11,10 @@
 #include "ErrorCodes.h"
 #include "PeopleCounter.h"
 #include "TofSensor.h"
+#include <StackArray.h>
 
+StackArray <int> stateStack;
+StackArray <int> tempStack;
 
 static int occupancyCount = 0;      // How many folks in the room or (if there is more than one door) - net occupancy through this door
 static int occupancyLimit = DEFAULT_PEOPLE_LIMIT;
@@ -35,47 +38,68 @@ PeopleCounter::~PeopleCounter() {
 void PeopleCounter::setup() {
 }
 
-void PeopleCounter::loop(){                // This function is only called if there is a change in occupancy state
-    static int oldOccupancyState = 0;       // Need to remember these for past state path
-    static bool atTheThreshold = false;
+void PeopleCounter::loop(){                                             // This function is only called if there is a change in occupancy state
     int oldOccupancyCount = occupancyCount;
-
-    switch (TofSensor::instance().getOccupancyState()) {
-
-      case 0:                               // No occupancy detected
-        oldOccupancyState = 0;
-        atTheThreshold = false;
-      break;
-
+    int newOccupancyState = TofSensor::instance().getOccupancyState();
+    int magicalStateMap[4] = {3, 2, 1, 0};                             // Define impossible state transitions (Ex. newOccupancyState cannot equal impossibilityMap[lastOccupancyState])
+    
+    switch(stateStack.count()){
+      case 0:
+        if(newOccupancyState == 0){                                     // First value MUST be a 0, ignore others
+          stateStack.push(newOccupancyState);   
+        }                                             
+        break;
       case 1:
-        if (atTheThreshold) {
-          atTheThreshold = false;
-          if (oldOccupancyState == 2) {
-            occupancyCount++; 
-          }
+        if(newOccupancyState != 0){                                     // Second state must NOT be a 0, ignore others
+          stateStack.push(newOccupancyState);                           // Push to the stack without checking for impossibilities
         }
-        oldOccupancyState = 1;
-      break;
-
+        break;                                                           
       case 2:
-        if (atTheThreshold) {
-          atTheThreshold = false;
-          if (oldOccupancyState == 1) {
-            occupancyCount--;
+      case 3:                                                           // When the stateStack has 2 or 3 items, we must identify impossible patterns and fix them.
+        if(stateStack.count() == 2 && newOccupancyState == 0){          // If we receive a 0 as the third state, the detected person left the area without walking in or out ...
+          while(stateStack.count() > 1){                                    // ... so reset the stateStack so it only contains 0.
+            stateStack.pop();                           // NOTE: IF IT IS COMMON FOR RANDOM 0s TO COME IN RIGHT HERE, MAY NEED TO LET THIS THROUGH AND FIX IT WITH THE NEXT CHANGE
           }
         }
-        oldOccupancyState = 2;
-      break;
-
-      case 3:
-        atTheThreshold = true;
-      break;
-
-      default:
-        #if PEOPLECOUNTER_DEBUG
-        Log.info("Error in occupancy state");
-        #endif
-      break;
+        tempStack.push(newOccupancyState);                              // Push the new occupancyState to the tempStack
+        while(stateStack.count() > 1){                                  // Go through the stack containing prior states
+          int current = stateStack.pop();                               
+          int after = tempStack.peek();
+          int before = stateStack.peek();
+          if(magicalStateMap[before] == current){                       // If the transition from before --> current is impossible, we must have failed to detect the person at some point ...
+            tempStack.push(current);                                            // ... so push current ...
+            int missedState = magicalStateMap[after];                               // ... consult the magical state map to determine what state was missed ...
+            tempStack.push(missedState);                                                // ... then push that.
+          } else if(magicalStateMap[current] == after) {                // If the transition from current --> after is impossible, we must have failed to detect the person at some point ...
+            int missedState = magicalStateMap[before];                          // ... so consult the magical state map to determine what state was missed ...
+            tempStack.push(missedState);                                            // ... push the missing state ...
+            tempStack.push(current);                                                    // ... then push current.
+          } else {                                                      // If the transition from before --> current is possible ...
+            tempStack.push(current);                                            // ... push current.
+          }
+        }
+        while(!tempStack.isEmpty()){                                            // Then move everything in the tempStack back to the permanent stack
+          stateStack.push(tempStack.pop());
+        }
+        break;
+      case 4:
+        if(newOccupancyState != 0){                                     // If the new occupancy state is NOT 0 ...
+          while(stateStack.peek() != newOccupancyState){                        // ... until the top of the stack is equal to the new occupancy state ...
+            stateStack.pop();                                                       // ... remove the top of the stack.
+          }
+        } else {                                                         // If the new occupancy state is 0 ...
+          stateStack.push(newOccupancyState);                                   // ... push the final state ...
+          char states[5];                                                           // ... turn it into a string by popping all values off the stack...
+          snprintf(states, sizeof(states), "%i%i%i%i%i", stateStack.pop(), stateStack.pop(), stateStack.pop(), stateStack.pop(), stateStack.pop());   
+          if(strcmp(states, "01320")){
+            occupancyCount++;                                                           // ... then increment the count if the sequence matches the increment secuence.
+          } else if(strcmp(states, "02310")) {
+            occupancyCount--;                                                           // ... then decrement the count if the sequence matches the decrement secuence.
+          } else {
+            Log.info("[ERROR WHEN COUNTING] Impossible sequence: %s", states);          // ... then throw an error if the sequence is supposed to be impossible.
+          }
+        }
+        break;
     }
 
    #if TENFOOTDISPLAY
